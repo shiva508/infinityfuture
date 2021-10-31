522ccd5f3934a0468d3740092b09c1d808cfae5f
shiva508 <dasarishiva1@gmail.com>
2021-10-31 11:26:59 +0530

    email configuratio

--- src/main/java/com/pool/model/LoginResponse.java
+++ src/main/java/com/pool/model/LoginResponse.java
@@ -3,21 +3,38 @@ package com.pool.model;
 public class LoginResponse {
 	private String userName;
 	private String jwtToken;
-	
+
+	private String firstName;
+
 	public LoginResponse() {
-		
+
 	}
+
 	public String getUserName() {
 		return userName;
 	}
-	public void setUserName(String userName) {
+
+	public LoginResponse setUserName(String userName) {
 		this.userName = userName;
+		return this;
 	}
+
 	public String getJwtToken() {
 		return jwtToken;
 	}
-	public void setJwtToken(String jwtToken) {
+
+	public LoginResponse setJwtToken(String jwtToken) {
 		this.jwtToken = jwtToken;
+		return this;
 	}
-	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public LoginResponse setFirstName(String firstName) {
+		this.firstName = firstName;
+		return this;
+	}
+
 }
